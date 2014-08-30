c8d112e6abfd3371fcb34b589ec068c7cd7f7fc2
Michael Gorsuch <gorsuch@simple.com>
2014-08-30 02:19:44 +0000

    move to native datastructures

--- rr.go
+++ rr.go
@@ -1,40 +1,43 @@
 package rr
 
-import "math"
+import (
+	"math"
+	"time"
+)
 
 type Bucket struct {
-	Epoch int64
-	Value *int64
+	T time.Time
+	V *int64
 }
 
 // TS represents a single time-series.
 type TS struct {
-	Resolution int64
+	Resolution time.Duration
 	Size       int64
 	Buckets    map[int64]*Bucket
 }
 
-func (t *TS) floor(epoch int64) int64 {
-	return epoch / t.Resolution * t.Resolution
+func (ts *TS) floor(t time.Time) time.Time {
+	return t.Round(ts.Resolution)
 }
 
-func (t *TS) index(epoch int64) int64 {
-	return int64(math.Mod(float64(t.floor(epoch)), float64(t.Size)))
+func (ts *TS) index(t time.Time) int64 {
+	return int64(math.Mod(float64(ts.floor(t).Unix()), float64(ts.Size)))
 }
 
-func (t *TS) Insert(epoch int64, value int64) {
-	p := &Bucket{t.floor(epoch), &value}
-	idx := t.index(p.Epoch)
-	t.Buckets[idx] = p
+func (ts *TS) Insert(t time.Time, value int64) {
+	p := &Bucket{ts.floor(t), &value}
+	idx := ts.index(p.T)
+	ts.Buckets[idx] = p
 }
 
 // TODO - test for buckets that are out of range
 // TODO - test for buckets with old epoch values
-func (t *TS) Get(epoch int64) *Bucket {
-	floor := t.floor(epoch)
-	idx := t.index(epoch)
+func (ts *TS) Get(t time.Time) *Bucket {
+	floor := ts.floor(t)
+	idx := ts.index(t)
 
-	bucket := t.Buckets[idx]
+	bucket := ts.Buckets[idx]
 	if bucket == nil {
 		return &Bucket{floor, nil}
 	}
@@ -43,13 +46,13 @@ func (t *TS) Get(epoch int64) *Bucket {
 }
 
 // TODO - test for start being greater than finish
-func (t *TS) GetRange(start int64, end int64) ([]*Bucket, error) {
+func (ts *TS) GetRange(start time.Time, end time.Time) ([]*Bucket, error) {
 	buckets := make([]*Bucket, 0)
-	start_floor := t.floor(start)
-	end_floor := t.floor(end)
+	start_floor := ts.floor(start)
+	end_floor := ts.floor(end)
 
-	for x := start_floor; x <= end_floor; x = x + t.Resolution {
-		bucket := t.Get(x)
+	for x := start_floor; x.Before(end_floor); x = x.Add(ts.Resolution) {
+		bucket := ts.Get(x)
 		if bucket == nil {
 			continue
 		}
--- rr_test.go
+++ rr_test.go
@@ -1,29 +1,26 @@
 package rr
 
 import (
-	"fmt"
 	"testing"
 	"time"
 )
 
 func TestInsert(t *testing.T) {
 	ts := TS{
-		Resolution: 10,
+		Resolution: 10 * time.Second,
 		Size:       60,
 		Buckets:    make(map[int64]*Bucket, 0),
 	}
 
 	now := time.Now()
-	epoch := now.Unix()
 
-	ts.Insert(epoch, 100)
-	if *ts.Get(epoch).Value != 100 {
+	ts.Insert(now, 100)
+	if *ts.Get(now).V != 100 {
 		t.Fatal("data was not stored")
 	}
 
-	buckets, _ := ts.GetRange(epoch, now.Add(4*time.Hour).Unix())
+	buckets, _ := ts.GetRange(now, now.Add(4*time.Hour))
 	if buckets == nil {
 		t.Fatal("no buckets found")
 	}
-	fmt.Println(len(buckets))
 }
